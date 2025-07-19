6df70eaaaf2e123e0d7c2324587e0faa1b7fb42f
kimheungseok <eovnfjfpa@naver.com>
2025-07-19 18:41:09 +0900

    인프런 복습 BFS_2 (이진트리 순회)

---
+++ src/inflearn/bfs_dfs/BFS_2.java
@@ -0,0 +1,53 @@
+package inflearn.bfs_dfs;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class BFS_2 {
+    static int n;
+    static boolean[] arr;
+    public static void main(String[] args) throws FileNotFoundException {
+        System.setIn(new FileInputStream("src/input.txt"));
+        Scanner sc = new Scanner(System.in);
+        Node root = new Node(1);
+        root.lt = new Node(2);
+        root.rt = new Node(3);
+        root.lt.lt = new Node(4);
+        root.lt.rt = new Node(5);
+        root.rt.lt = new Node(6);
+        root.rt.rt = new Node(7);
+
+        BFS(root);
+
+    }
+    static void BFS(Node root){
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()){
+            int len = queue.size();
+            for(int i = 0 ; i < len; i ++){
+                Node poll = queue.poll();
+                System.out.print(poll.data + " ");
+                if (poll.lt != null){
+                    queue.offer(poll.lt);
+                }
+                if (poll.rt != null){
+                    queue.offer(poll.rt);
+                }
+            }
+        }
+    }
+    static class Node{
+        int data;
+        Node lt, rt;
+
+        public Node(int data) {
+            this.data = data;
+            this.lt = null;
+            this.rt = null;
+        }
+    }
+}
