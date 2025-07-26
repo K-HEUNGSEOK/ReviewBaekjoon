f0df46104fec33ec39441e1ec025e126ce2536e6
kimheungseok <eovnfjfpa@naver.com>
2025-07-26 17:38:35 +0900

    7일차 다시풀기 Stack 크레인 인형뽑기

---
+++ src/inflearn/problems_to_retry/_7day/Stack.java
@@ -0,0 +1,49 @@
+package inflearn.problems_to_retry._7day;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Stack {
+    public static void main(String[] args) throws FileNotFoundException {
+        System.setIn(new FileInputStream("src/input.txt"));
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[][] arr = new int[n+1][n+1];
+
+        for(int i = 1; i <= n ; i ++){
+            for(int j = 1; j <=n ; j ++){
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        int m = sc.nextInt();
+        int[] command = new int[m];
+        for(int i = 0 ; i < m ; i ++) command[i] = sc.nextInt();
+
+        java.util.Stack<Integer> stack = new java.util.Stack<>();
+        int ans = 0;
+        for(int i = 0 ; i < m ; i ++){
+            int c = command[i];
+            int cnt = 1;
+
+            while (cnt < n && arr[cnt][c] == 0){
+                cnt ++;
+            }
+
+            //나온 인형
+            int doll = arr[cnt][c];
+
+            if (!stack.isEmpty()&& stack.peek() == doll){
+                ans +=2;
+                stack.pop();
+            }else{
+                stack.push(doll);
+            }
+
+            arr[cnt][c] = 0;
+
+        }
+        System.out.println(ans);
+    }
+}
