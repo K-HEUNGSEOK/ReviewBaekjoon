d0093ba342a38458cc8498c3f2d372b50a54c0f8
K-HEUNGSEOK <eovnfjfpa@naver.com>
2025-08-01 12:13:44 +0900

    DFS_9 조합의 경우수 메모제이션

---
+++ src/inflearn/bfs_dfs/DFS_9.java
@@ -0,0 +1,26 @@
+package inflearn.bfs_dfs;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class DFS_9 {
+    static int[][] arr;
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("src/input.txt"));
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int r = sc.nextInt();
+        arr = new int[n+1][r+1];
+        System.out.println(DFS(n, r));
+    }
+    static int DFS(int n , int r){
+        if (arr[n][r] != 0) return arr[n][r];
+        if (n == r || r == 0){
+            return 1;
+        }
+        else{
+            return arr[n][r] = DFS(n-1,r-1) + DFS(n-1,r);
+        }
+    }
+}
